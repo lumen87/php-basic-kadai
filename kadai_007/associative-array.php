b57a31647b4bbacd2ab735b52224e0dfb1a9dbf5
lumen87 <ymmt78mrk@gmail.com>
2025-12-05 21:15:43 +0900

    7章課題提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<html>
+  <meta charset="UTF-8">
+  <title>PHPの基礎を学ぼう 7章課題</title>
+</html>
+
+<body>
+  <p>
+      <?php
+      //連想配列に値を代入する
+      $items_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+      //連想配列の値を出力する
+      print_r($items_data);
+      ?>
+  </p>
+</body>
+
+</html>
